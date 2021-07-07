79405cb89e71843dafeeb6bffab58b95e32523a8
Jafar TP <jafar@lithos.in>
2021-07-07 20:35:19 +0530

    initial commit

---
+++ lib/src/capability_profile.dart
@@ -0,0 +1,69 @@
+import 'dart:convert' show json;
+import 'package:flutter/services.dart' show rootBundle;
+
+class CodePage {
+  CodePage(this.id, this.name);
+  int id;
+  String name;
+}
+
+class CapabilityProfile {
+  CapabilityProfile._internal(this.name, this.codePages);
+
+  /// Public factory
+  static Future<CapabilityProfile> load({String name = 'default'}) async {
+    final content = await rootBundle
+        .loadString('packages/esc_pos_utils/resources/capabilities.json');
+    Map capabilities = json.decode(content);
+
+    var profile = capabilities['profiles'][name];
+
+    if (profile == null) {
+      throw Exception("The CapabilityProfile '$name' does not exist");
+    }
+
+    List<CodePage> list = [];
+    profile['codePages'].forEach((k, v) {
+      list.add(CodePage(int.parse(k), v));
+    });
+
+    // Call the private constructor
+    return CapabilityProfile._internal(name, list);
+  }
+
+  String name;
+  List<CodePage> codePages;
+
+  int getCodePageId(String? codePage) {
+    if (codePages == null) {
+      throw Exception("The CapabilityProfile isn't initialized");
+    }
+
+    return codePages
+        .firstWhere((cp) => cp.name == codePage,
+        orElse: () => throw Exception(
+            "Code Page '$codePage' isn't defined for this profile"))
+        .id;
+  }
+
+  static Future<List<dynamic>> getAvailableProfiles() async {
+    final content = await rootBundle
+        .loadString('packages/esc_pos_utils/resources/capabilities.json');
+    Map capabilities = json.decode(content);
+
+    var profiles = capabilities['profiles'];
+
+    List<dynamic> res = [];
+
+    profiles.forEach((k, v) {
+      res.add({
+        'key': k,
+        'vendor': v['vendor'] is String ? v['vendor'] : '',
+        'model': v['model'] is String ? v['model'] : '',
+        'description': v['description'] is String ? v['description'] : '',
+      });
+    });
+
+    return res;
+  }
+}
---
+++ test/flutter_esc_pos_utils_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_esc_pos_utils/flutter_esc_pos_utils.dart';
+
+void main() {
+  test('adds one to input values', () {
+
+  });
+}
